b1f20a0a53be6a2fe0671df78cd60929ba23347d
Pedrorcpla <pedrorcpla@gmail.com>
2022-07-27 14:19:29 -0300

    Criada migration Produtos

---
+++ app/Database/Migrations/2022-07-27-170802_Produtos.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Produtos extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'              => 'int',
+                'auto_increment'    => true
+            ],
+            'name' => [
+                'type'          =>  'varchar',
+                'constraint'    =>  100
+            ],
+            'description' => [
+                'type'  =>  'text',
+            ],
+            'valor' => [
+                'type' => 'decimal(10,2)',
+            ],
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('tb_produto');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('tb_produto');
+    }
+}
